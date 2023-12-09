fe35afcdd94f16b08772ec7b110c108817f9c12c
GabrielAlmeida1701 <gabriel.almeida1701@gmail.com>
2023-12-09 02:32:08 -0300

    sqlite adaptation

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -2,12 +2,8 @@ import { DataSource, DataSourceOptions } from "typeorm";
 
 //npm run migration:generate -- src/db/migrations/InitialMigrations
 export const dataSourceOptions: DataSourceOptions = {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '1234',
-    database: 'nestdb',
+    type: 'sqlite',
+    database: './src/db/database.db',
     entities: [ 'dist/**/*.entity.js' ],
     migrations: [ 'dist/db/migrations/*.js']
 }
